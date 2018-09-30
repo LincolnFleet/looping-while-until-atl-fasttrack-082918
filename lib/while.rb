fdb17a9b400d37f4b33c4208b7b6615675e13b8c
Travis Martin <travisemerymartin@gmail.com>
2018-09-30 15:26:54 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,9 +2,6 @@ def using_while
   levitation_force = 6
   while levitation_force < 10
   levitation_force += 1
-	p levitation_force
+	p "Wingardium Leviosa"
 	end
-  while levitation_force == 10
-  p "Wingardium Leviosa"
-end
 end
